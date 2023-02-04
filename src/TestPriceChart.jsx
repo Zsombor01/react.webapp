6fbd3114d6a792fe903a359703bc4efc66db2eeb
Bíró Zsombor <bzsozso01@gmail.com>
2023-02-04 22:09:39 +0100

    Finally added a chart Library, learning to use it

--- src/TestPriceChart.jsx
+++ src/TestPriceChart.jsx
@@ -1,47 +1,94 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Chart } from 'react-chartjs-2';
-import LineChart from './components/LineChart';
+import { Line } from 'react-chartjs-2';
+import {
+	Chart as ChartJS,
+	LineElement,
+	CategoryScale, //x axis
+	LinearScale, //y axis
+	PointElement,
+	Legend,
+	Tooltip,
+} from 'chart.js';
+
+ChartJS.register(
+	LineElement,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	Legend,
+	Tooltip
+);
 
 export default function TestPriceChart() {
-	const [historicalPrices, setHistoricalPrices] = useState([]);
-	const [logPrices, setLogPrices] = useState({
-		labels: historicalPrices.map((price) => price[1]),
+	const [date, setDate] = useState([]);
+	const [price, setPrice] = useState([]);
+	const data = {
+		labels: date,
 		datasets: [
 			{
-				label: 'Valami majd lesz itt',
-				data: historicalPrices.map((price) => price[0]),
-				backgroundColor: [
-					'rgba(75,192,192,1)',
-					'#ecf0f1',
-					'#50AF95',
-					'#f3ba2f',
-					'#2a71d0',
-				],
+				label: 'Previous prices',
+				data: price,
+				backgroundColor: 'grey',
 				borderColor: 'black',
-				borderWidth: 2,
+				pointBorderColor: 'black',
+				fill: false,
+				tension: 0.4, //for the line to be more 'curvier'
 			},
 		],
-	});
+	};
+	const options = {
+		radius: 0,
+		hitRadius: 30,
+		plugins: {
+			legend: true,
+		},
+		scales: {
+			x: {
+				grid: {
+					display: false,
+				},
+			},
+			y: {
+				grid: {
+					display: false,
+				},
+			},
+		},
+		grid: {
+			display: false,
+		},
+	};
 
 	useEffect(() => {
 		fetch(
-			'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=10&interval=daily'
+			'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=30&interval=fourhour'
 		)
 			.then((res) => res.json())
 			.then((data) => {
-				setHistoricalPrices(data.prices);
+				setDate(
+					data.prices.map((date) => {
+						return date[0];
+					})
+				);
+				setPrice(
+					data.prices.map((price) => {
+						return price[1];
+					})
+				);
 			});
-		// if u log inside the useEffect u will se the result of what u logged out in the previous cycle
+		// if u log inside the useEffect u will see the result of what u logged out in the previous cycle
 	}, []);
-	console.log(historicalPrices);
+	// console.log(date);
+	// console.log(price);
+
 	/* Making a chart */
 
 	return (
 		<div className='TestPriceChartDiv'>
 			<p>THis is a test line </p>
 			<div style={{ width: 700 }}>
-				<LineChart chartData={logPrices} />
+				<Line data={data} options={options}></Line>
 			</div>
 		</div>
 	);
